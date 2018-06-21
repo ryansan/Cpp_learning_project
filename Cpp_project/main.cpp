35affec010e6b3d60669efcace180e362ef85bbf
ryansan <31653675+ryansan@users.noreply.github.com>
2018-06-21 21:26:31 +0200

    Initial Commit

---
+++ Cpp_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp_project
+//
+//  Created by Ryan Sangha on 21/06/2018.
+//  Copyright © 2018 Ryan Sangha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
